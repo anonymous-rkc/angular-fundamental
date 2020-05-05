666d2c61fd96dbf17d8f1d994ac5f373e23b1a1a
Rahul Kumar <36847653+anonymous-rkc@users.noreply.github.com>
2020-05-06 01:44:41 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<app-events-list></app-events-list>`
+})
+export class AppComponent {
+  title = 'ng-fundamentals';
+}
---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
+import { EventsThumbnailComponent } from './events/events-thumbnail/events-thumbnail.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventsThumbnailComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
